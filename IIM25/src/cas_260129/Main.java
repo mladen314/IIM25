410f39739674f025e4f6ec2bbbe9c36c0780ba77
mladen314 <144899141+mladen314@users.noreply.github.com>
2026-02-05 11:00:47 +0100

    Update Main.java

--- IIM25/src/cas_260129/Main.java
+++ IIM25/src/cas_260129/Main.java
@@ -10,16 +10,17 @@ public class Main {
 		
 		System.out.println("Unijeti cijeli broj");
 		int a = input.nextInt();
-		
+		int n = 3;
 		System.out.println("Suma cifara broja "+a+" je "+suma_cifara(a));
-		
+		System.out.println("Otpornost broja "+a+" je "+otpornost_broja(a));
+		System.out.println("Najmanji broj otpornosti "+n+" je "+broj_otpornosti_n(n));
 	}
 	
 	/*
 	 * Funkcija koja vraca sumu cifara
 	 */
 	
-	public static int suma_cifara(int n) {
+	public static int suma_cifara(long n) {
 		int sum=0;
 		if(n<0) {
 			n=-n;
@@ -34,8 +35,30 @@ public class Main {
 	
 	
 	/*
-	 * Vraca otpornost broja koja se racuna kao broj transformacija da se dobije
-	 * jednocifren broj, broj se transformise u sumu svojih cifara.
+	 * Vraca otpornost broja koja se racuna kao broj transformacija 
+	 * da se dobije jednocifren broj, broj se transformise u 
+	 * sumu svojih cifara.
 	 */
+	
+	public static int otpornost_broja(long x) {
+		int otp = 0;
+		while(x>9) {
+			otp++;
+			x=suma_cifara(x);
+		}
+		return otp;		
+	}
+	
+	/*
+	 * Vraca prvi prirodni broj sa otpornoscu n
+	 */
+	
+	public static long broj_otpornosti_n(int n) {
+		long br=1;
+		while(otpornost_broja(br)<n) {
+			br++;
+		}
+		return br;
+	}
 
 }
