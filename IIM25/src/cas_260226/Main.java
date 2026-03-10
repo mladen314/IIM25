437ffae2a091bd1d23c534b141e462bddc19c793
mladen314 <mladen.jankovic.314@gmail.com>
2026-03-10 14:15:14 +0100

    Update Main.java

--- IIM25/src/cas_260226/Main.java
+++ IIM25/src/cas_260226/Main.java
@@ -6,7 +6,24 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		int [] niz;     // ili int niz[];
+		niz = new int[10];
+		niz[0] = 15;
+		niz[1] = 10;
+		niz[2] = niz[0]+niz[1];
+		int k = 5;
+		niz[k] = niz[2]*10;
 		
+		System.out.println(niz[9]);
+		
+		for (int i = 0; i < niz.length; i++) {
+			System.out.println("vrijevnos elementa na "+i+"-toj poziciji je "+niz[i]);
+		}
+		
+		double [] x = {15.2, 17.4, 21.3, 31.5};
+		
+		for (double element : x) {
+			System.out.print(element+" ");
+		}
 
 	}
 
